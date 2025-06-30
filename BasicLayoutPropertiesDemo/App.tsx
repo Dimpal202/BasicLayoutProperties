c32fb7f4613a321d1599445dd70d3f702f388ea3
Dimpal202 <dongadimpal20@gmail.com>
2025-06-30 16:56:59 +0530

    BasicLayoutProperties

---
+++ BasicLayoutPropertiesDemo/App.tsx
@@ -0,0 +1,243 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <View style={{ alignItems: "center" }}>
+                <Text style={{ fontWeight: "bold" }}>Flex Basis</Text>
+                <View style={{ borderWidth: 1, flexWrap: "wrap", flexDirection: "row" }}>
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green", flexBasis: 50 }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+                </View>
+            </View>
+
+            {/* default properties 0 Flex Grow*/}
+            <View style={{ alignItems: "center" }}>
+                <Text style={{ fontWeight: "bold" }}>Flex Grow</Text>
+                <View style={{ borderWidth: 1, flexWrap: "wrap", flexDirection: "row" }}>
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green", flexGrow: 1 }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+                </View>
+            </View>
+            {/* default properties 1 Flex Shrink*/}
+            <View style={{ alignItems: "center" }}>
+                <Text style={{ fontWeight: "bold" }}>Flex Shrink</Text>
+                <View style={{ borderWidth: 1, flexWrap: "wrap", flexDirection: "row" }}>
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green", flexShrink: 1 }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+
+
+                    <View style={{ backgroundColor: "red" }}>
+                        <Text>one</Text>
+                    </View>
+                    <View style={{ backgroundColor: "green" }}>
+                        <Text>two</Text>
+                    </View>
+                    <View style={{ backgroundColor: "blue" }}>
+                        <Text>three</Text>
+                    </View>
+                    <View style={{ backgroundColor: "yellow" }}>
+                        <Text>four</Text>
+                    </View>
+                    <View style={{ backgroundColor: "pink" }}>
+                        <Text>five</Text>
+                    </View>
+                </View>
+            </View>
+
+
+            {/* width */}
+            <View style={{ flexDirection: "row", marginTop: 40 }}>
+                <View style={{ backgroundColor: "red", width: 100 }}>
+                    <Text>one</Text>
+                </View>
+                <View style={{ backgroundColor: "green", maxWidth: 100 }}>
+                    <Text>two</Text>
+                </View>
+                <View style={{ backgroundColor: "blue" }}>
+                    <Text>three</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+
+export default App
